f96d2996d68b7ac0838b1dcddd3dbb64a4065bee
Tygo Willems <tawillems@hotmail.com>
2019-12-02 14:00:40 +0100

    1ste commit

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,9 @@
 //javascript code
+
+function myFunction()
+{
+    var myInput=document.getElementById("myInput").value;
+    if (myInput < 25){
+        document.getElementById("myOutput").innerHTML="Sorry, je saldo is te laag";
+    }
+}
