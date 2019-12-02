08095387ec2163aaa7b47e08389d03ce1d1d4c12
Tygo Willems <tawillems@hotmail.com>
2019-12-02 14:31:31 +0100

    OPDR 2

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -4,6 +4,9 @@ function myFunction()
 {
     var myInput=document.getElementById("myInput").value;
     if (myInput < 25){
-        document.getElementById("myOutput").innerHTML="Sorry, je saldo is te laag";
+        document.getElementById("myOutput").innerHTML="Sorry, je hebt te weinig saldo!";
+    }
+    if (myInput >= 25){
+        document.getElementById("myOutput").innerHTML="Je hebt voldoende saldo om € 25 op te nemen"; 
     }
 }
